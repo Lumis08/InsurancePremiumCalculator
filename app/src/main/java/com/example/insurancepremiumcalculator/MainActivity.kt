69d4b5c7454273c763ea3f2ccc3640fd65034cec
Lumis08 <limbin234@gmail.com>
2019-12-09 13:04:17 +0800

    Init

--- app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
+++ app/src/main/java/com/example/insurancepremiumcalculator/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var premium: PremiumViewModel
-    // Hello
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
